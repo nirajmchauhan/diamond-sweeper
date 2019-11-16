a7ca355283fbb540c36ffb6e991eff6ff22efd2c
Niraj Chauhan <nchauhan@equalexperts.com>
2019-11-16 18:38:57 +0530

    add babel & integrate it with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,14 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     port: 3000
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
   }
 };
